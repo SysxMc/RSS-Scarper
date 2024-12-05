f0578667af225e7fe52f6316d0e8879e56ae7640
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-12-05 15:59:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from sub import *
 import csv
+import asyncio
+import os
+import random
+import requests
+from bs4 import BeautifulSoup
+import logging
+import subprocess
+import json
 
 feed_filename = "links.txt"
 
