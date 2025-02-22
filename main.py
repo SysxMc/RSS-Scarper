190070efca1b3d4d8848d06711d4b5d5026ce1c3
StupidFucker79 <StupidSeleniumTest+7688@protonmail.com>
2025-02-22 12:36:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,8 @@ async def fetch_all():
 
 async def main():
     new_links = await fetch_all()
-    all_links = set()
+    print(new_links)
+  
     if os.path.exists(feed_filename):
         with open(feed_filename, 'r') as feed:
             all_links = set(feed.read().splitlines())
